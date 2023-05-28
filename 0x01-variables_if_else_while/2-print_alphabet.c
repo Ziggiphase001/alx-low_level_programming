aba80b77d542f2ddba274a859c2ae115b2523ef6
Ziggiphase001 <abdulbasit1900550@st.futminna.edu.ng>
2023-05-28 12:10:56 -0700

    C program that prints all lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char Alpha;
+
+	for (Alpha = 'a'; Alpha <= 'z'; Alpha++)
+		putchar(Alpha);
+
+			putchar('\n');
+
+			return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,3 +1,4 @@
 The C Variables, If, Else, While task
 This C program shows if a number is positive or negative
 The C program print the last number of a number
+Add C program that prints all lowercase
