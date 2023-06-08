93da6ea53982adbbc0b2f2f5e23b86ad25fe94a8
Ziggiphase001 <abdulbasit1900550@st.futminna.edu.ng>
2023-06-08 04:29:43 -0700

    This directory contains contents related to Project 0x06. C - More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * rot13 - check the code for Holberton School students.
+ * @s: the string passed
+ *
+ * Return: the string pointer.
+ */
+
+char *rot13(char *s)
+{
+	int x;
+	char *ta = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *rot = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char *rtn = s;
+
+	while (*s)
+	{
+		x = 0;
+		while (ta[x])
+		{
+			if (*s == ta[x])
+			{
+				*s = rot[x];
+				break;
+			}
+			x++;
+		}
+		s++;
+	}
+	return (rtn);
+}
