e1f541e5532303a21f6ba5cd793bd6705e780595
Ziggiphase001 <abdulbasit1900550@st.futminna.edu.ng>
2023-06-09 09:57:31 -0700

    Added 0x07-pointer projects

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - sets value of pointer to a char
+ * @s: the string
+ * @to: the value to set it to
+ *
+ * Return: Always 0.
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
